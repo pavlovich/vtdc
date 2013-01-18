fe305ca93ba2fdb91f60e4195288e4ab540297f5
Peter Pavlovich <pavlovich@gmail.com>
2013-01-17 23:12:59 -0500

    initial commit

---
+++ config/hobo_routes.rb
@@ -0,0 +1,16 @@
+# This is an auto-generated file: don't edit!
+# You can add your own routes in the config/routes.rb file
+# which will override the routes in this file.
+
+Vtdc::Application.routes.draw do
+
+
+
+# These are the Hobo 1.3 style routes.   They've been included for backwards
+# compatibility reasons because the names of some named routes have been changed.
+# Please update your views to use the new named route names and then disable this
+# section by setting config.hobo.dont_emit_deprecated_routes = true.  blah blah
+
+ 
+ 
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vtdc::Application.config.secret_token = '7c605e475f298059eed62df87d741f2caef0ca85877038f3b6bd638f7705915b1d522bbcce3dbefbac6e5e44bf9e23cb927f59cc6201f79dc41540ed9e71ca8b'
