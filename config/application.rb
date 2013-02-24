567b757695d34724e359437a079bc22cd4d451be
Peter Pavlovich <pavlovich@gmail.com>
2013-02-24 03:48:38 -0500

    try to fix styles

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,7 @@ module Vtdc
     config.hobo.dont_emit_deprecated_routes = true
     # Hobo: the public subsite loads public.css & public.js
     config.assets.precompile += %w(public.css public.js)
+    config.assets.precompile += %w(vtdc.css)
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
