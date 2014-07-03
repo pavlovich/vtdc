7734a04fb8bc7e6da9d8903fc417457229c6fcf5
Peter Pavlovich <pavlovich@gmail.com>
2014-07-02 22:14:28 -0400

    added ability to view past events

---
+++ app/controllers/past_events_controller.rb
@@ -0,0 +1,41 @@
+class PastEventsController
+
+  hobo_model_controller
+
+  auto_actions [:index, :show]
+
+  before_filter :login_required, :except => [ :index, :show ]
+
+
+  def hobo_index
+    # full_calendar will hit the index method with query parameters
+    # 'start' and 'end' in order to filter the results for the
+    # appropriate month/week/day.  It should be possiblt to change
+    # this to be starts_at and ends_at to match rails conventions.
+    # I'll eventually do that to make the demo a little cleaner.
+    @events = Event.scoped
+    @events = @events.order("starts_at")
+    @events = @events.past(Time.now)
+    @events = @events.after(params['start']) if (params['start'])
+    @events = @events.before(params['end']) if (params['end'])
+    @events = @events.paginate(:page => params[:page])
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @events }
+      format.js  { render :json => @events }
+    end
+  end
+
+  # GET /events/1
+  # GET /events/1.xml
+  def hobo_show
+    @event = Event.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @event }
+      format.js { render :json => @event.to_json }
+    end
+  end
+
+end
